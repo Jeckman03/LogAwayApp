873f37cc117d7bc2d6aa81dc4069bc876a280a89
Jeff Eckman <jeckman03@gmail.com>
2025-12-01 22:09:31 -0700

    Add DataLibrary project with database access functionality
    Created a new `DataLibrary` project targeting .NET 9.0. Added support for database operations using `Dapper` and `Microsoft.Data.SqlClient`. Implemented `IDataAccess` interface and `SqlDb` class for executing stored procedures. Added `UserData` class for user-related operations and models for `UserModel` and `LogModel`. Updated solution file to include the new project and configurations.

---
+++ DataLibrary/Data/UserData.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using DataLibrary.Db;
+using DataLibrary.Models;
+using System.Data;
+
+namespace DataLibrary.Data
+{
+    public class UserData
+    {
+        private readonly IDataAccess _dataAccess;
+        private readonly ConnectionStringData _connectionString;
+
+        public UserData(IDataAccess dataAccess, ConnectionStringData connectionString)
+        {
+            _dataAccess = dataAccess;
+            _connectionString = connectionString;
+        }
+
+        public async Task<int> CreateUser(UserModel user)
+        {
+            DynamicParameters p = new DynamicParameters();
+
+            p.Add("FirstName", user.FirstName);
+            p.Add("LastName", user.LastName);
+            p.Add("StartDate", user.StartDate);
+            p.Add("Id", DbType.Int32, direction: ParameterDirection.Output);
+
+            await _dataAccess.SaveData("dbo.spUser_Insert",
+                                       p,
+                                       _connectionString.SqlConnectionName);
+
+            return p.Get<int>("Id");
+        }
+
+        public Task<int> UpdateUser(UserModel user)
+        {
+            return _dataAccess.SaveData("dbo.spUser_Update",
+                                        new { Id = user.Id, FirstName = user.FirstName, LastName = user.LastName },
+                                        _connectionString.SqlConnectionName);
+        }
+
+        public Task<int> DeleteUser(int userId)
+        {
+            return _dataAccess.SaveData("dbo.spUser_Delete",
+                                        new { Id = userId },
+                                        _connectionString.SqlConnectionName);
+        }
+
+        public async Task<UserModel> GetUserById(int userId)
+        {
+            var recs = await _dataAccess.LoadData<UserModel, dynamic>("spUserGetById",
+                                                                      new { Id = userId },
+                                                                      _connectionString.SqlConnectionName);
+
+            return recs.FirstOrDefault();
+        }
+    }
+}
+
---
+++ DataLibrary/Models/LogModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DataLibrary.Models
+{
+    public class LogModel
+    {
+        public int Id { get; set; }
+        public DateTime LogDate { get; set; }
+        public string LogEntry { get; set; }
+        public int UserId { get; set; }
+    }
+}
